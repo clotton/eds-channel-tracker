be4371fdf1072667cb2e1810e3a75bf3cd7a68e8
clotton <clotton@adobe.com>
2025-03-18 10:32:27 -0400

    remove api key field

--- slack-admin/slack.js
+++ slack-admin/slack.js
@@ -4,19 +4,9 @@ import { API_ENDPOINT } from './config.js';
 const allSlackChannels = document.getElementById('myslackchannels');
 const slackChannelsContainer = document.getElementById('slack-channels-container');
 
-const key = document.getElementById('api_key');
-
-const persistFormFields = () => {
-  localStorage.setItem('key', key.value);
-};
-
 const getAllSlackChannels = async () => {
   try {
-    const response = await fetch(`${API_ENDPOINT}`, {
-      headers: {
-        'x-api-key': key.value,
-      }
-    });
+    const response = await fetch(`${API_ENDPOINT}`);
 
     if (response.ok) {
       const channels = await response.json();
@@ -26,19 +16,6 @@ const getAllSlackChannels = async () => {
 
   return [];
 }
-
-const refreshSaveButton = () => {
-  const button = document.getElementById('save');
-
-  const add = slackChannelsContainer.querySelectorAll('.add');
-  const remove = slackChannelsContainer.querySelectorAll('.remove');
-
-  if (add.length || remove.length) {
-    button.removeAttribute('disabled');
-  } else {
-    button.setAttribute('disabled', true);
-  }
-};
 
 const displayChannels = async () => {
   slackChannelsContainer.innerHTML = '<span class="spinner"></span>';
@@ -67,46 +44,6 @@ const displayChannels = async () => {
     ul.appendChild(li);
 
   });
-
-  const button = document.createElement('button');
-  button.id = 'save';
-  button.textContent = 'Save';
-  button.disabled = true;
-  button.classList.add('button');
-
-  button.addEventListener('click', async () => {
-    button.disabled = true;
-    button.innerHTML = '<span class="spinner"></span>';
-    const add = slackChannelsContainer.querySelectorAll('.add');
-
-    const body = {
-      add: [],
-      remove: [],
-    };
-
-    add.forEach(async (li) => {
-      const displayName = li.querySelector('h4').textContent;
-      body.add.push(displayName);
-    });
-
-
-    const remove = slackChannelsContainer.querySelectorAll('.remove');
-
-    remove.forEach(async (li) => {
-      const name = li.querySelector('h4').textContent;
-      body.remove.push(name);
-    });
-
-    displayChannels();
-  });
-
-  const wrapper = document.createElement('p');
-  wrapper.classList.add('button-wrapper');
-  wrapper.appendChild(button);
-
-  slackChannelsContainer.innerHTML = '';
-  slackChannelsContainer.appendChild(ul);
-  slackChannelsContainer.appendChild(wrapper);
 }
 
 /**
@@ -115,10 +52,6 @@ const displayChannels = async () => {
  */
 allSlackChannels.addEventListener('click', async (e) => {
   e.preventDefault();
-  persistFormFields();
-
   displayChannels();
 });
-
-key.value = localStorage.getItem('key') || '';
 
